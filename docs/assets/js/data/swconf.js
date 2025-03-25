21c521f2eb23203f5692f313f8c14cddd22334f8
junsu <zzansu01@gmail.com>
2025-03-25 10:50:59 +0900

    .

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742835813',resources: [
+    cacheName: 'chirpy-1742867452',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
